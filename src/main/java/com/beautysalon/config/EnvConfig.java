b7310dbab51d37cf2c57b2d3dd18153bddb83e97
bogdanbt <bt.tarasenko@gmail.com>
2025-04-28 12:41:10 +0200

    change envconfig2

--- src/main/java/com/beautysalon/config/EnvConfig.java
+++ src/main/java/com/beautysalon/config/EnvConfig.java
@@ -1,9 +1,12 @@
 package com.beautysalon.config;
 
-
 public class EnvConfig {
 
     public static String get(String key) {
-        return System.getenv(key);
+        String value = System.getenv(key);
+        if (value == null) {
+            throw new RuntimeException("Environment variable '" + key + "' is not set!");
+        }
+        return value;
     }
 }
